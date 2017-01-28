a861b7863de0bbd3ee82e64ff6187e72833dacef
Akinorev <gomezgomezveronica@gmail.com>
2017-01-28 16:02:07 +0100

    Adding C# behaviour to the needle object

---
+++ Pin the circle/Assets/Scripts/Needle Scripts/NeedleMovementScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleMovementScript : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject needleBody;
+
+    private bool canShootCircle;
+    private bool touchedTheCircle;
+
+    private float speed = 70f;
+
+    private Rigidbody2D myBody;
+
+    void Awake ()
+    {
+
+    }
+
+    void Initialize()
+    {
+        needleBody.SetActive(false);
+    }
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
