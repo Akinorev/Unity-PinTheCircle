ce810fa01bb3d050a82e1945ece45d298e5cc214
Akinorev <gomezgomezveronica@gmail.com>
2017-01-28 17:59:57 +0100

    Added needle behaviour now it moves on the Y axis

--- Pin the circle/Assets/Scripts/Needle Scripts/NeedleMovementScript.cs
+++ Pin the circle/Assets/Scripts/Needle Scripts/NeedleMovementScript.cs
@@ -6,29 +6,41 @@ public class NeedleMovementScript : MonoBehaviour {
     [SerializeField]
     private GameObject needleBody;
 
-    private bool canShootCircle;
+    private bool canFireNeedle = false;
     private bool touchedTheCircle;
 
-    private float speed = 70f;
+    private float forceY = 70f;
 
     private Rigidbody2D myBody;
 
     void Awake ()
     {
-
+        Initialize();
     }
 
     void Initialize()
     {
         needleBody.SetActive(false);
+        myBody = GetComponent<Rigidbody2D>();
     }
+
 	// Use this for initialization
 	//void Start () {
 	
 	//}
 	
 	// Update is called once per frame
-	//void Update () {
-	
-	//}
+	void Update () {
+	    if (canFireNeedle)
+        {
+            myBody.velocity = new Vector2(0, forceY);
+        }
+	}
+
+    public void FireTheNeedle()
+    {
+        needleBody.SetActive(true);
+        myBody.isKinematic = false;
+        canFireNeedle = true;
+    }
 }
