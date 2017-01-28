eb6b31724a10e02e89886cc8582cb57d454c91ef
Akinorev <gomezgomezveronica@gmail.com>
2017-01-28 21:11:24 +0100

    Setted game manager, created shoot button and created array for multiple needles

---
+++ Pin the circle/Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    private Button shootButton;
+
+    [SerializeField]
+    private GameObject needle;
+
+    private GameObject[] needles;
+
+    [SerializeField]
+    private int howManyNeedles;
+
+    private float needleDistance = 1.5f;
+    private int needleIndex;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
